940183151f4b1615c8a7f12644c4631099170828
avish-meliodas <61935571+avish-meliodas@users.noreply.github.com>
2025-05-19 11:48:14 +0900

    Update tasks.py to Asynchrounous running in background tasks

--- app/tasks.py
+++ app/tasks.py
@@ -1,14 +1,21 @@
 from uuid import uuid4
+from fastapi import FastAPI, BackgroundTasks
+from app.model_pred import prediction
 
 results = {}
 
-def async_predict(data):
-    from app.model_pred import prediction
+def run_model(data, job_id):
+    response = prediction(data)
+    results[job_id] = response 
+
+def async_predict(data, background_tasks: BackgroundTasks):
+    
     job_id = str(uuid4())
-
-    result = prediction(data)
-
-    results[job_id] = result
+    results[job_id] = None
+    
+    # result = prediction(data)
+    background_tasks.add_task(run_model, data, job_id)
+    # results[job_id] = result
 
     return job_id
 
